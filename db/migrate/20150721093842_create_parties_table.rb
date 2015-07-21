6323c69d331e755722b1f1ee3de199cddb3f8ed9
rumama <rumama@users.noreply.github.com>
2015-07-21 11:07:53 +0100

    edit table migrations

--- db/migrate/20150721093842_create_parties_table.rb
+++ db/migrate/20150721093842_create_parties_table.rb
@@ -3,9 +3,9 @@ class CreatePartiesTable < ActiveRecord::Migration
   	create_table :parties do |table|
   		table.string :name, null: false
   		table.string :address, null: false
-  		table.string :coordinates, null: false
-  		table.date :date, null: false
-  		table.time :time, null: false
+  		table.string :lat, null: false
+  		table.string :long, null: false
+  		table.datetime :starts_at, null: false
   		table.timestamps
 		end
   end
