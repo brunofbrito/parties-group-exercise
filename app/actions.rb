a33991cda2eaabc657e0384e4f914f3d4f97aee7
ricardojusto <ricardojusto@outlook.com>
2015-07-21 15:17:12 +0100

    add the create function and button

--- app/actions.rb
+++ app/actions.rb
@@ -6,11 +6,26 @@ get '/' do
   @parties = Party.all
   erb :index
 end
+# form to create a new party
+get '/new' do
+	erb :new
+end
 
+# method to save a new party, the /new route should point here
+post '/create' do
+	Party.create({
+		name: params[:name],
+		address: params[:address],
+		lat: params[:lat],
+		long: params[:long],
+		starts_at: params[:starts_at]
+		})
+	redirect '/'
+end
 # show individual post
 get '/:id' do
-		@party = Party.find(params[:id])
-		erb :show
+	@party = Party.find(params[:id])
+	erb :show
 end
 
 # form to edit a single party
@@ -25,13 +40,7 @@ post '/:id/edit' do
 	redirect '/'
  end
 
-# form to create a new party
-get '/new' do
-end
 
-# method to save a new party, the /new route should point here
-post '/create' do
-end
 
 # method to update an existing party, the /:id/edit should point here
 post '/:id/update' do
