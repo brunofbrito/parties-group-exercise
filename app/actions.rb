35a724044c2e4e817c180217369c4e8dc52dfe7c
Bruno Brito <brunofbrito@gmail.com>
2015-07-21 10:20:23 +0100

    Exercise structure added to actions.rb

--- app/actions.rb
+++ app/actions.rb
@@ -1,8 +1,29 @@
-get "/" do
-  erb :index
+# list of all parties
+get '/' do
+  ...
 end
 
-get "/posts" do
-  @posts = Post.all
-  erb :"posts/index"
+# show individual post
+get '/:id' do
+  ...
+end
+
+# form to edit a single party
+get '/:id/edit' do
+  ...
+end
+
+# form to create a new party
+get '/new do
+  ...
+end
+
+# method to save a new party, the /new route should point here
+post '/create' do
+  ...
+end
+
+# method to update an existing party, the /:id/edit should point here
+post '/:id/update' do
+  ...
 end
