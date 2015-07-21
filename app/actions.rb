51e511c3c58398897014db3fd37e11114ba25d51
rumama <rumama@users.noreply.github.com>
2015-07-21 10:24:26 +0100

    fix errors

--- app/actions.rb
+++ app/actions.rb
@@ -1,29 +1,23 @@
 # list of all parties
 get '/' do
-  ...
 end
 
 # show individual post
 get '/:id' do
-  ...
 end
 
 # form to edit a single party
 get '/:id/edit' do
-  ...
 end
 
 # form to create a new party
-get '/new do
-  ...
+get '/new' do
 end
 
 # method to save a new party, the /new route should point here
 post '/create' do
-  ...
 end
 
 # method to update an existing party, the /:id/edit should point here
 post '/:id/update' do
-  ...
 end
